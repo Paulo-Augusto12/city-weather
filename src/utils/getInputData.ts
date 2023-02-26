7527d8310c6da817f3519cfa987da71dd4d13e77
Paulo Augusto <paulo-augusto12@outlook.com>
2023-02-26 16:58:45 -0300

    feat: versão inicial da aplicação

--- src/utils/getInputData.ts
+++ src/utils/getInputData.ts
@@ -1,7 +1,7 @@
 import { key } from "../api/apikey";
 import axios from "axios";
 async function getData(input: String) {
-  const api = `http://api.openweathermap.org/geo/1.0/direct?q=${input}&limit=5&appid=${key}`;
+  const api = `https://api.openweathermap.org/geo/1.0/direct?q=${input}&limit=5&appid=${key}`;
 
   const response = await axios.get(api);
 
